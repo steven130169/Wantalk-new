538b36630c34a2ecc2bc709d46d46a061c4d8608
chiwen <chiwen.cheng@e-utc.com.tw>
2018-08-01 13:29:43 +0800

    chiwen joomla template

---
+++ onepagetemplate/index.php
@@ -0,0 +1,33 @@
+<?php defined('_JEXEC') or die; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>我的公司</title>
+    <!-- Bootstrap的css樣式 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- 自訂的css樣式 -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- jQuery函式庫 --><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<!-- Bootstrap的js函式庫 -->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+    <!-- 自訂的js程式碼檔案 -->
+    <script src="js/custom.js"></script>
+</head>
+<body>
+	<!-- 第1個區塊 - 首頁 -->
+	<div id="home" class="home">
+	  <div class="text-vcenter">
+	    <h1>歡迎!</h1>
+	    <h3>這是一個使用Bootstrap3設計的網站</h3>
+	    <a href="#about" class="btn btn-default btn-lg">關於我們</a>
+	  </div>
+	</div>
+	<!-- 第1個區塊 -->
+</body>
+</html>
